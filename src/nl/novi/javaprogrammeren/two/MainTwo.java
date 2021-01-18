632f84c5cdd15073ebf7632528f39db7de8db44f
Nick Stuivenberg <n.stuivenberg@novi.nl>
2021-01-18 11:29:12 +0100

    Antwoorden voor de scope-opdracht

--- src/nl/novi/javaprogrammeren/two/MainTwo.java
+++ src/nl/novi/javaprogrammeren/two/MainTwo.java
@@ -4,10 +4,14 @@ import java.util.Random;
 
 public class MainTwo {
     public static void main(String[] args) {
+        int totalsum = 0;
         for (int i = 0; i < 10; i++) {
-            int totalsum = 0;
+            // int totalsum heeft hier een scope binnen de for-lus. We willen hem echter buiten de for-lus kunnen
+            // aanroepen. Daarom verplaatsen we de declaratie naar buiten de for-lus.
+            // int totalsum = 0;
             Random random = new Random();
             totalsum = totalsum + random.nextInt();
         }
+        System.out.println("De som van alle random-getallen is: " + totalsum);
     }
 }
